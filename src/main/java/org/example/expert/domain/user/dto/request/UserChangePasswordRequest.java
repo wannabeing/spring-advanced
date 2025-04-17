644e35b339eacda898679bd06199e434abf430d6
wannabeing <wannabeing@naver.com>
2025-04-17 14:54:31 +0900

    refactor: [Lv1] Validation 활용하여 코드개선

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
+    @NotBlank(message = "현재 비밀번호는 필수 값입니다.")
     private String oldPassword;
-    @NotBlank
+
+    @NotBlank(message = "새 비밀번호는 필수 값입니다.")
+    @Size(min = 8, message = "새 비밀번호는 최소 8자 이상입니다.")
+    @Pattern(
+        regexp = "^(?=.*[A-Z])(?=.*\\d).*$",
+        message = "새 비밀번호는 숫자와 대문자를 포함해야 합니다."
+    )
     private String newPassword;
 }

4bf47fe6c60ba93c0cceea4b1f2b8e6799ffd4ec
wannabeing <wannabeing@naver.com>
2025-04-18 22:25:31 +0900

    refactor: [Lv4] 관리자 유저 Controller API 테스트 추가

--- src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
@@ -15,7 +15,8 @@ public class UserAdminController {
     private final UserAdminService userAdminService;
 
     @PatchMapping("/admin/users/{userId}")
-    public void changeUserRole(@PathVariable long userId, @RequestBody UserRoleChangeRequest userRoleChangeRequest) {
+    public String changeUserRole(@PathVariable long userId, @RequestBody UserRoleChangeRequest userRoleChangeRequest) {
         userAdminService.changeUserRole(userId, userRoleChangeRequest);
+        return "OK";
     }
 }
